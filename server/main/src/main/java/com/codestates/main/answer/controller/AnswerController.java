2cac63ad80908074fdc0950bd03af304b848bba0
soobinkim-kor <shb03207@naver.com>
2022-09-19 17:58:30 +0900

    백엔드: Dev 브랜치 생성

---
+++ server/main/src/main/java/com/codestates/main/answer/controller/AnswerController.java
@@ -0,0 +1,27 @@
+package com.codestates.main.answer.controller;
+
+import com.codestates.main.answer.dto.AnswerDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/answer")
+public class AnswerController {
+
+    @GetMapping("/a")
+    public String getAnswers(){
+        return "answers";
+    }
+
+    @GetMapping("/b")
+    public String getAnswer(){
+        return "answer";
+    }
+
+    @GetMapping("/c")
+    public String getAnswerByQuestion(@ModelAttribute AnswerDTO.GET answerDto){
+        System.out.println(answerDto.getPage()+" "+answerDto.getSize()+" "+answerDto.getCategory());
+        return "answer";
+    }
+
+
+}
---
+++ server/main/src/main/java/com/codestates/main/answer/service/AnswerService.java
@@ -0,0 +1,7 @@
+package com.codestates.main.answer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnswerService {
+}
