415e71ec9d8fc65e6b5fa98c8e01d88ffb105b39
kchs94 <kchs94@gmail.com>
2022-09-23 16:52:47 +0900

    Refactor: PostPostDto 수정

--- server/main/src/main/java/com/codestates/main/post/dto/PostPostDto.java
+++ server/main/src/main/java/com/codestates/main/post/dto/PostPostDto.java
@@ -1,4 +1,26 @@
 package com.codestates.main.post.dto;
 
+import com.codestates.main.post.entity.BoardType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PostPostDto {
+
+//    private Long postId;
+
+    private String title;
+
+    private String content;
+
+//    private BoardType boardType;
+
+//    public String getBoardType() {
+//        return boardType.getName();
+//    }
 }
