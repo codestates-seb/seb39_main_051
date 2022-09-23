2ef06ac88c38009024e163e92cf301ccf0736e0b
LeeChangHoon <changhoon1030@gmail.com>
2022-09-23 17:37:53 +0900

    Fix : Router 페이지로 이름 교체에 따른 수정

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,12 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import PostPage from './pages/PostPage';
+import Post from './pages/Post';
+import AnswerPage from './pages/AnswerPage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import SubscribePage from './pages/SubscribePage';
+
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/post' element={<PostPage />} />
+      <Route path='/answer' element={<AnswerPage />} />
+      <Route path='/board' element={<Post />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/signup' element={<SignUpPage />} />
+      <Route path='/subscribe' element={<SubscribePage/>} />
     </Routes>
   );
 };
