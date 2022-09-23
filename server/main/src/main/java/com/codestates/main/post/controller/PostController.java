b949ec218ba1f4ae321dcb8e4a801531dc207562
kchs94 <kchs94@gmail.com>
2022-09-23 14:19:45 +0900

    Add: 게시글 Controller 핸들러 메서드 시그니처 생성

--- server/main/src/main/java/com/codestates/main/post/controller/PostController.java
+++ server/main/src/main/java/com/codestates/main/post/controller/PostController.java
@@ -4,9 +4,7 @@ import com.codestates.main.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -16,5 +14,34 @@ public class PostController {
 
     private final PostService postService;
 
+    @PostMapping
+    public ResponseEntity postPost() {
+        System.out.println("PostController.postPost");
+        return null;
+    }
 
+    @PatchMapping("/{post-id}")
+    public ResponseEntity patchPost(@PathVariable("post-id") Long postId) {
+        System.out.println("PostController.patchPost");
+        return null;
+    }
+
+    @GetMapping("/{post-id}")
+    public ResponseEntity getPost() {
+        System.out.println("PostController.getPost");
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity getPosts(@RequestParam int page,
+                                   @RequestParam int size) {
+        System.out.println("PostController.getPosts");
+        return null;
+    }
+
+    @DeleteMapping("/{post-id}")
+    public ResponseEntity deletePost() {
+
+        return null;
+    }
 }
