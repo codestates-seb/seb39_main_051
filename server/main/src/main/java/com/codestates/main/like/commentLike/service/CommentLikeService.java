6f2b0cbf785613aecc77c950f5c6c445eb5987bb
Choongseop Kim <kchs94@gmail.com>
2022-09-29 02:56:13 +0900

    Add: CommentLikeService 생성

---
+++ server/main/src/main/java/com/codestates/main/like/commentLike/service/CommentLikeService.java
@@ -0,0 +1,47 @@
+package com.codestates.main.like.commentLike.service;
+
+import com.codestates.main.comment.entity.Comment;
+import com.codestates.main.comment.service.CommentService;
+import com.codestates.main.like.commentLike.entity.CommentLike;
+import com.codestates.main.like.commentLike.repository.CommentLikeRepository;
+import com.codestates.main.member.entity.Member;
+import com.codestates.main.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CommentLikeService {
+
+    private final CommentLikeRepository commentLikeRepository;
+    private final CommentService commentService;
+    private final MemberService memberService;
+    public void commentLike(Long commentId, Long memberId) {
+        Comment findComment = commentService.findComment(commentId);
+        Member findMember = memberService.findVerifiedMember(memberId);
+        Optional<CommentLike> optionalCommentLike = commentLikeRepository.findByCommentAndMember(findComment, findMember);
+
+        optionalCommentLike.ifPresentOrElse(
+                commentLike -> {
+                    commentLikeRepository.delete(commentLike);
+                    findComment.discount(commentLike);
+                    findComment.updateLikeCount();
+                },
+
+                () -> {
+                    CommentLike commentLike = CommentLike.builder().build();
+
+                    commentLike.setComment(findComment);
+                    commentLike.setMember(findMember);
+                    findComment.updateLikeCount();
+
+                    commentLikeRepository.save(commentLike);
+                }
+        );
+
+    }
+}
