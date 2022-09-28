0d6bfb78200322b64d9b861c9d941ec4628d655e
Choongseop Kim <kchs94@gmail.com>
2022-09-29 02:55:19 +0900

    Add: CommentLike 엔티티 생성

---
+++ server/main/src/main/java/com/codestates/main/like/commentLike/entity/CommentLike.java
@@ -0,0 +1,45 @@
+package com.codestates.main.like.commentLike.entity;
+
+import com.codestates.main.comment.entity.Comment;
+import com.codestates.main.like.postlike.entity.PostLike;
+import com.codestates.main.member.entity.Member;
+import com.codestates.main.post.entity.Post;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Optional;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class CommentLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentLikeId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "POST_ID")
+    private Comment comment;
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.setCommentLikes(this);
+    }
+
+    public void setComment(Comment comment) {
+        this.comment = comment;
+        comment.setCommentLikes(this);
+    }
+
+    public static boolean isVotedComment(Optional<CommentLike> optionalCommentLike) {
+        return optionalCommentLike.isPresent();
+    }
+}
