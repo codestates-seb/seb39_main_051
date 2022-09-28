db24eca485d395c2fe1a3d5a23604bec664c81ba
kchs94 <kchs94@gmail.com>
2022-09-28 20:27:24 +0900

    Add: AnswerResponseDto2

--- server/main/src/main/java/com/codestates/main/answer/dto/AnswerResponseDto2.java
+++ server/main/src/main/java/com/codestates/main/answer/dto/AnswerResponseDto2.java
@@ -1,6 +1,8 @@
 package com.codestates.main.answer.dto;
 
 import com.codestates.main.answer.entity.Answer;
+가import com.codestates.main.comment.dto.CommentResponseDto;
+import com.codestates.main.comment.dto.CommentResponseDto;
 import com.codestates.main.member.entity.Member;
 import com.codestates.main.question.entity.Question;
 import lombok.AllArgsConstructor;
@@ -9,21 +11,35 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class AnswerResponseDto2 {
+    private Long answerId;
     private String content;
 
-    private String answerWriter;
+    private String email;
+    private Long memberId;
+    private String nickname;
 
     private LocalDateTime createdAt;
 
+    private List<CommentResponseDto> comments;
+
     public AnswerResponseDto2(Answer answer) {
+        this.answerId = answer.getAnswerId();
+        this.email = answer.getMember().getEmail();
+        this.memberId = answer.getMember().getMemberId();
         this.content = answer.getContent();
-        this.answerWriter = answer.getMember().getNickname();
+        this.nickname = answer.getMember().getNickname();
         this.createdAt = answer.getCreatedAt();
+        this.comments = answer.getComments()
+                .stream()
+                .map(comment -> new CommentResponseDto(comment))
+                .collect(Collectors.toList());
     }
 }
