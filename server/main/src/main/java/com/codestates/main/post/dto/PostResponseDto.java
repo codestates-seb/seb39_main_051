5d4b5948f89cd33caba926b04b0957ad2947ee10
kchs94 <kchs94@gmail.com>
2022-09-28 11:58:26 +0900

    Add: PostResponseDto

--- server/main/src/main/java/com/codestates/main/post/dto/PostResponseDto.java
+++ server/main/src/main/java/com/codestates/main/post/dto/PostResponseDto.java
@@ -1,6 +1,7 @@
 package com.codestates.main.post.dto;
 
 import com.codestates.main.comment.entity.Comment;
+import com.codestates.main.post.entity.Post;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,15 +19,23 @@ public class PostResponseDto {
 
     private String title;
 
-    private String content;
-
-    private String type;
-
     private String category;
+
+    private String nickname;
+
+    private String email;
+
+    private Long memberId;
 
     private LocalDateTime createdAt;
 
-    private LocalDateTime modifiedAt;
-
-    private List<Comment> comments;
+    public PostResponseDto(Post post) {
+        this.postId = post.getPostId();
+        this.title = post.getTitle();
+        this.category = post.getCategory();
+        this.nickname = post.getMember().getNickname();
+        this.email = post.getMember().getEmail();
+        this.memberId = post.getMember().getMemberId();
+        this.createdAt = post.getCreatedAt();
+    }
 }
