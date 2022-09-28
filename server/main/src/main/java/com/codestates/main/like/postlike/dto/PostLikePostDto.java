36f44d93936043c2092bd99a062b10680c198bbd
Choongseop Kim <kchs94@gmail.com>
2022-09-29 02:09:25 +0900

    Add: PostLikePostDto 생성

---
+++ server/main/src/main/java/com/codestates/main/like/postlike/dto/PostLikePostDto.java
@@ -0,0 +1,15 @@
+package com.codestates.main.like.postlike.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostLikePostDto {
+
+    private Long memberId;
+}
