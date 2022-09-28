3094bec76b1b70b6ca43188f563c27b0fa1e1418
kchs94 <kchs94@gmail.com>
2022-09-28 20:24:52 +0900

    Add: Answer 일대다 매핑 추가

--- server/main/src/main/java/com/codestates/main/answer/entity/Answer.java
+++ server/main/src/main/java/com/codestates/main/answer/entity/Answer.java
@@ -1,21 +1,22 @@
 package com.codestates.main.answer.entity;
 
 import com.codestates.main.auditing.BaseEntity;
+import com.codestates.main.comment.entity.Comment;
 import com.codestates.main.member.entity.Member;
 import com.codestates.main.question.entity.Question;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Answer extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,6 +32,9 @@ public class Answer extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "QUESTION_ID")
     private Question question;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "answer")
+    private List<Comment> comments = new ArrayList<>();
 
     public void addQuestion(Question question) {
         this.question = question;
