e92a296c2b7694047f58c2641c604d8502e0ef37
kchs94 <kchs94@gmail.com>
2022-09-28 11:58:34 +0900

    Add: PostResponseDto2 생성

---
+++ server/main/src/main/java/com/codestates/main/post/dto/PostResponseDto2.java
@@ -0,0 +1,51 @@
+package com.codestates.main.post.dto;
+
+import com.codestates.main.comment.dto.CommentResponseDto;
+import com.codestates.main.post.entity.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostResponseDto2 {
+    private Long postId;
+
+    private String title;
+
+    private String content;
+    private String category;
+
+    private String nickname;
+
+    private Long memberId;
+
+    private String email;
+
+    private int likes = 3;
+    private LocalDateTime createdAt;
+
+    private List<CommentResponseDto> comments;
+
+    public PostResponseDto2(Post post) {
+        this.postId = post.getPostId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.category = post.getCategory();
+        this.nickname = post.getMember().getNickname();
+        this.memberId = post.getMember().getMemberId();
+        this.email = post.getMember().getEmail();
+        this.createdAt = post.getCreatedAt();
+        this.comments = post.getComments()
+                .stream()
+                .map(comment -> new CommentResponseDto(comment))
+                .collect(Collectors.toList());
+    }
+}
