883a4c638789029fb3223dae86a017d23e8e5d95
Choongseop Kim <kchs94@gmail.com>
2022-09-29 03:34:47 +0900

    Add: AnswerLikePostDto 생성

---
+++ server/main/src/main/java/com/codestates/main/like/answerlike/dto/AnswerLikePostDto.java
@@ -0,0 +1,15 @@
+package com.codestates.main.like.answerlike.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnswerLikePostDto {
+
+    private Long memberId;
+}
