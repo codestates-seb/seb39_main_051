8ec987fb38e2f7b07dc075111988a9928217faea
kchs94 <kchs94@gmail.com>
2022-09-28 11:53:45 +0900

    Add: AnswerPostDto 생성

---
+++ server/main/src/main/java/com/codestates/main/answer/dto/AnswerPostDto.java
@@ -0,0 +1,17 @@
+package com.codestates.main.answer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AnswerPostDto {
+
+    private Long memberId;
+    private Long questionId;
+    private String content;
+}
