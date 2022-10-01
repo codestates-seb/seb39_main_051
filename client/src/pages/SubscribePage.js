b158edc4cfd19d9eebed74b824cd6f0909159086
LeeChangHoon <changhoon1030@gmail.com>
2022-10-01 11:26:35 +0900

    Feat : 구독페이지 API 연결

--- client/src/pages/SubscribePage.js
+++ client/src/pages/SubscribePage.js
@@ -3,80 +3,105 @@ import BorderLayout from '../components/BorderLayout';
 import CategoryCard from '../components/CategoryCard';
 import { useSelector } from 'react-redux';
 import 'react-toastify/dist/ReactToastify.css';
-import { toast, ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
+import Toast from '../components/Toast';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const SubscribePage = () => {
   const themeState = useSelector((state) => state.themeSlice).theme;
+  const { isLoggedIn, userId, nickName } = useSelector((state) => state.userInfoSlice);
+  const [subscribeArr, setSubscribeArr] = useState([1,2,3,4,5,6,7,8]);
   const arr = [
-    '리액트',
-    '자바스크립트',
-    '자바',
-    '스프링',
-    '자료구조',
-    '네트워크',
-    '데이터베이스',
-    '운영체제'
+    { categoryName: 'React', questionCategoryId: 2 },
+    { categoryName: 'Javascript', questionCategoryId: 8 },
+    { categoryName: 'Java', questionCategoryId: 1 },
+    { categoryName: 'Spring', questionCategoryId: 3 },
+    { categoryName: 'Data Structure', questionCategoryId: 4 },
+    { categoryName: 'OS', questionCategoryId: 5 },
+    { categoryName: 'Database', questionCategoryId: 6 },
+    { categoryName: 'Network', questionCategoryId: 7 },
   ];
-  const mySubscribe = [ '리액트', '자바스크립트']
 
-  const handleSubscribe = (category) => {
-    toast.success(`${category}를 구독합니다!`,{
-      position:toast.POSITION.TOP_RIGHT,
-      className: 'toast-message'
-    })
-
+  const handleSubscribe = (id, categoryName, isSubscribe) => {
+    if(isSubscribe){
+      //구독해제상황
+      // await axios.post('/member/subscription',{
+      //   questionCategoryId:id
+      // })
+      toast.success(`${categoryName} 구독을 해제합니다!`)
+      const origin = subscribeArr
+      setSubscribeArr(origin.filter((el)=>el !== id))
+    }
+    else{
+      if(categoryName==='Spring'){
+        // await axios.post('/member/subscription',{
+        //   questionCategoryId:id
+        // })
+        toast.success(`${categoryName}을 구독합니다!`)
+        const origin = subscribeArr
+        origin.push(id)
+        setSubscribeArr([...origin])
+      }else{
+        // await axios.post('/member/subscription',{
+        //   questionCategoryId:id
+        // })
+        toast.success(`${categoryName}를 구독합니다!`)
+        const origin = subscribeArr
+        origin.push(id)
+        setSubscribeArr([...origin])
+      }
+    }
   }
+
+  // useEffect(()=>{
+  //   axios.get('/subscription')
+  //   .then((res)=>console.log(res))
+  // })
   return (
     <BorderLayout>
-      <Message themeState={themeState}>매일 받고 싶은 면접 주제를 선택하세요!</Message>
-        <StyledContainer themeState={themeState} />
-        <GridLayout>
-          {arr.map((el)=><CategoryCard name={el} handleClick={handleSubscribe} isSubscribe={mySubscribe.includes(el)}/>)}
-        </GridLayout>
+      <Message themeState={themeState}>
+        매일 받고 싶은 면접 주제를 선택하세요!
+      </Message>
+      <Toast />
+      <GridLayout>
+        {arr.map((el,idx) => (
+          <CategoryCard
+            key={el.questionCategoryId}
+            categoryName={el.categoryName}
+            questionCategoryId={el.questionCategoryId}
+            handleClick={handleSubscribe}
+            isSubscribe={subscribeArr.includes(el.questionCategoryId)}
+          />
+        ))}
+      </GridLayout>
     </BorderLayout>
   );
 };
 
-const StyledContainer = styled(ToastContainer)`
-  // https://styled-components.com/docs/faqs#how-can-i-override-styles-with-higher-specificity
-  &&&.Toastify__toast-container {
+const GridLayout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-row-gap: 20%;
+  height: auto;
+  place-items: center;
+  @media screen and (max-width: 1024px) {
+    grid-template-columns: repeat(2, 50%);
+    grid-row-gap: 10%;
+    padding-bottom: 20%;
   }
-  .Toastify__toast {
-    background-color: ${(props)=>props.themeState==='light'? 'var(--color-orange)' : 'var(--color-gray)'};
-    color:var(--color-white);
-  }
-  .Toastify__toast-body {
-  }
-  .Toastify__progress-bar {
-    background-color:  ${(props) => props.themeState==='light' ? 'var(--color-yellow)': 'var(--color-black)'};
+  @media screen and (max-width: 413px) {
+    grid-template-columns: repeat(1, 100%);
+    grid-row-gap: 2.5%;
+    padding-bottom: 40%;
   }
 `;
-
-
-const GridLayout = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4, 25%);
-    grid-row-gap: 20%;
-    height:auto;
-    place-items: center;
-    @media screen and (max-width: 1024px) {
-      grid-template-columns: repeat(2, 50%);
-      grid-row-gap: 10%;
-      padding-bottom: 20%;
-    }
-    @media screen and (max-width: 413px) {
-      grid-template-columns: repeat(1, 100%);
-      grid-row-gap: 2.5%;
-      padding-bottom: 40%;
-    }
-`
 const Message = styled.div`
-  text-align:center;
+  text-align: center;
   font-weight: bold;
   font-size: 1vw;
-  margin-bottom:3%;
-  color:${(props)=>props.themeState ==='light' === 'var(--color-black)'};
-`
-
+  margin-bottom: 3%;
+  color: ${(props) => (props.themeState === 'light') === 'var(--color-black)'};
+`;
 
 export default SubscribePage;
