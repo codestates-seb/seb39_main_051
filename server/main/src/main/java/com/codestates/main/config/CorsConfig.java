231cae8d2d91cb1adeae0466328a3ef0780fce59
soobinkim-kor <shb03207@naver.com>
2022-10-05 17:43:09 +0900

    Fix : corsconfig 수정

--- server/main/src/main/java/com/codestates/main/config/CorsConfig.java
+++ server/main/src/main/java/com/codestates/main/config/CorsConfig.java
@@ -15,7 +15,6 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOriginPattern("*");
-        config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("Authorization");
